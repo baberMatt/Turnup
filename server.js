5eaf5386a2c08fc46349268e999e4fb205f4575e
Matt Baber <mbaber1142@gmail.com>
2021-12-21 11:31:24 -0500

    server and pkg heroku configured

--- server.js
+++ server.js
@@ -67,6 +67,10 @@ app.post('/login', function (req, res, next) {
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 // Add routes, both API and view
 app.use(routes);
